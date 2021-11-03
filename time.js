d7f3f26fcee25380741eb56bdc083c7ba10612f4
ShneurNovack <81717928+ShneurNovack@users.noreply.github.com>
2021-11-02 23:34:05 -0400

    Update time.js

--- time.js
+++ time.js
@@ -36,9 +36,7 @@ function Time() {
   second = update(second);
 
   // Adding time elements to the div
-  document.getElementById("digital-clock").innerText = hour + " : " + minute;
-  document.getElementById("seconds").innerText = second;
-  document.getElementById("period").innerText = period;
+  document.getElementById("digital-clock").innerText = hour + " : " + minute + " : " + second + " : " + period;
 
 
   // Set Timer to 1 sec (1000 ms)
